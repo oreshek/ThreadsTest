b5ed055c6f5aca706f6b78ee829fe59889154a7d
Oleg <oreshek1111@yandex.ru>
2020-08-07 20:36:46 +0300

    threads

---
+++ MessageProcessingDemo.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ThreadTest
+{
+    class MessageProcessingDemo
+    {
+
+        public ConcurrentQueue<string> messageQueue = new ConcurrentQueue<string>();
+
+
+        public void GenerateMessage()
+        {
+            Thread.Sleep(10);
+            messageQueue.Enqueue($"{DateTime.UtcNow} : {Guid.NewGuid()}");
+
+        }
+
+        public string ProcessOneMessage()
+        {
+            // long running operatione
+            Thread.Sleep(20);
+            messageQueue.TryDequeue(out string result);
+            return result;
+
+        }
+
+        public async Task<IEnumerable<string>> ProcessMultipleMessages(int numOfTasksToRun = 1)
+        {
+            Console.WriteLine(messageQueue.Count);
+            List<Task<string>> listTask = new List<Task<string>>();
+
+            for (int i = 0; i < numOfTasksToRun; i++)
+            {
+                listTask.Add(Task.Run(ProcessOneMessage));
+            }
+
+            var results = await Task.WhenAll(listTask.ToArray());
+            return results;
+
+        }
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Threading;
+
+namespace ThreadTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MessageProcessingDemo demo = new MessageProcessingDemo();
+
+            demo.GenerateMessage();
+            var a = demo.ProcessOneMessage();
+            Console.WriteLine(a);
+
+            Thread genMessage = new Thread(() =>
+            {
+                while (true)
+                {
+                    demo.GenerateMessage();
+                }
+            });
+            Thread procMessage = new Thread(() =>
+            {
+                while (true)
+                {
+                    Stopwatch stopWatch = new Stopwatch();
+                    List<string> resultMessages = new List<string>();
+                    var messageCount = demo.messageQueue.Count;
+                    //Console.WriteLine(messageCount);
+                    if (messageCount > 50)
+                    {
+                        stopWatch.Start();
+                        var resultMessage = demo.ProcessMultipleMessages(30).Result;
+                        stopWatch.Stop();
+                        TimeSpan ts = stopWatch.Elapsed;
+                        string elapsedTime = String.Format("{0:00}:{1:00}:{2:00}.{3:00}",
+            ts.Hours, ts.Minutes, ts.Seconds,
+            ts.Milliseconds);
+                        Console.WriteLine("RunTime " + elapsedTime);
+                    }
+                    //else
+                    //{
+                    //    var resultMessage = demo.ProcessOneMessage();
+                    //}
+                    //foreach(var result in resultMessages)
+                    //{
+                    //    Console.WriteLine(resultMessages);
+                    //}
+                    //Console.WriteLine(resultMessage);
+                }
+            });
+
+            genMessage.Start();
+            procMessage.Start();
+            //Thread.Sleep(1000);
+            ////var resultMessage = demo.ProcessMessage();
+            //Console.WriteLine(demo.messageQueue.Count);
+        }
+    }
+}
